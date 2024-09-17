4647108021aaa12e99f17d683939f98f00ee0205
Ayush <ayushnoorani6@gmail.com>
2024-09-17 21:43:47 +0530

    Add connection to db and setup tables

---
+++ backend/db/makeTables.js
@@ -0,0 +1,63 @@
+const pool = require("./connect.js");
+
+const createTableNoOfWheels = async () => {
+  const query = `
+    CREATE TABLE IF NOT EXISTS NoOfWheels (
+      id SERIAL PRIMARY KEY,
+      wheels INT NOT NULL
+    );
+  `;
+  try {
+    await pool.query(query);
+    console.log("Number Of Wheels Table created successfully.");
+  } catch (error) {
+    console.error("Error creating table number of wheels:", error.stack);
+  }
+};
+
+const createTableVehicleType = async () => {
+  const query = `
+      CREATE TABLE IF NOT EXISTS VehicleType (
+        id SERIAL PRIMARY KEY,
+        type VARCHAR(100) NOT NULL,
+        wheelId INT,
+        FOREIGN KEY (wheelId) REFERENCES NoOfWheels(id)
+      );
+    `;
+  try {
+    await pool.query(query);
+    console.log("Vehicle Type Table created successfully.");
+  } catch (error) {
+    console.error("Error creating table vehicle type:", error.stack);
+  }
+};
+
+const createTableVehicleModel = async () => {
+  const query = `
+        CREATE TABLE IF NOT EXISTS VehicleModel (
+          id SERIAL PRIMARY KEY,
+          model VARCHAR(100) NOT NULL,
+          vehicleType INT,
+          FOREIGN KEY (vehicleType) REFERENCES VehicleType(id)
+        );
+      `;
+  try {
+    await pool.query(query);
+    console.log("Vehicle Model Table created successfully.");
+  } catch (error) {
+    console.error("Error creating table vehicle model:", error.stack);
+  }
+};
+
+const setupDB = async () => {
+  try {
+    await createTableNoOfWheels();
+    await createTableVehicleType();
+    await createTableVehicleModel();
+    console.log("Tables created successfully");
+  } catch (e) {
+    console.log("Error creating tables: ", e);
+  }
+};
+
+module.exports = setupDB;
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const APIroutes = require("./routes/APIroutes");
 const cors = require("cors");
 require("dotenv").config();
+const setupDB = require("./db/makeTables.js");
 
 const port = 3001;
 
@@ -13,7 +14,14 @@ app.use("/", APIroutes);
 
 const start = () => {
   try {
-    app.listen(port, console.log("Listening on port " + port));
+    app.listen(port, async () => {
+      console.log("Listening on port " + port);
+      try {
+        await setupDB();
+      } catch (e) {
+        console.log("Erro when trying to setup database tables", e);
+      }
+    });
   } catch (error) {
     console.log(error);
   }
