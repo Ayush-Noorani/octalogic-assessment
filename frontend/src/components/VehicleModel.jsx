b2c56942eba66bb87a85158280c159382f22f68d
Ayush <ayushnoorani6@gmail.com>
2024-09-18 13:56:01 +0530

    Add loading state

--- frontend/src/components/VehicleModel.jsx
+++ frontend/src/components/VehicleModel.jsx
@@ -3,34 +3,44 @@ import React, { useEffect, useState } from "react";
 const VehicleModel = ({ VehicleType, updateBookingInformation }) => {
   const [vehicleModels, setVehicleModels] = useState([]);
   const [selectedOption, setSelectedOption] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleModelSelection = (e) => {
     updateBookingInformation(e.target.name, e.target.value);
     setSelectedOption(e.target.value);
   };
   useEffect(() => {
+    setLoading(true);
     fetch(`http://localhost:3001/getVehileModel/${VehicleType}`)
       .then((res) => res.json())
-      .then((data) => setVehicleModels(data.data));
+      .then((data) => {
+        setVehicleModels(data.data);
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
-      {vehicleModels.length &&
-        vehicleModels.map((vehicle, index) => {
-          return (
-            <div key={index}>
-              <input
-                type="radio"
-                name="VehicleModel"
-                value={vehicle.model}
-                checked={selectedOption == vehicle.model}
-                onChange={handleModelSelection}
-              />
-              <label>{vehicle.model}</label>
-            </div>
-          );
-        })}
+      {loading && <h3>Please wait ... </h3>}
+      {!loading && (
+        <>
+          {vehicleModels.length &&
+            vehicleModels.map((vehicle, index) => {
+              return (
+                <div key={index}>
+                  <input
+                    type="radio"
+                    name="VehicleModel"
+                    value={vehicle.model}
+                    checked={selectedOption == vehicle.model}
+                    onChange={handleModelSelection}
+                  />
+                  <label>{vehicle.model}</label>
+                </div>
+              );
+            })}
+        </>
+      )}
     </div>
   );
 };
--- frontend/src/components/VehicleType.jsx
+++ frontend/src/components/VehicleType.jsx
@@ -3,34 +3,44 @@ import React, { useEffect, useState } from "react";
 const VehicleType = ({ Wheels, updateBookingInformation }) => {
   const [vehicleTypes, setVehicleTypes] = useState([]);
   const [selectedOption, setSelectedOption] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleTypeSelection = (e) => {
     updateBookingInformation(e.target.name, e.target.value);
     setSelectedOption(e.target.value);
   };
   useEffect(() => {
+    setLoading(true);
     fetch(`http://localhost:3001/getVehileType/${Wheels}`)
       .then((res) => res.json())
-      .then((data) => setVehicleTypes(data.data));
+      .then((data) => {
+        setVehicleTypes(data.data);
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
-      {vehicleTypes.length &&
-        vehicleTypes.map((vehicle, index) => {
-          return (
-            <div key={index}>
-              <input
-                type="radio"
-                name="VehicleType"
-                value={vehicle.type}
-                checked={selectedOption == vehicle.type}
-                onChange={handleTypeSelection}
-              />
-              <label>{vehicle.type}</label>
-            </div>
-          );
-        })}
+      {loading && <h3>Please wait ... </h3>}
+      {!loading && (
+        <>
+          {vehicleTypes.length &&
+            vehicleTypes.map((vehicle, index) => {
+              return (
+                <div key={index}>
+                  <input
+                    type="radio"
+                    name="VehicleType"
+                    value={vehicle.type}
+                    checked={selectedOption == vehicle.type}
+                    onChange={handleTypeSelection}
+                  />
+                  <label>{vehicle.type}</label>
+                </div>
+              );
+            })}
+        </>
+      )}
     </div>
   );
 };
